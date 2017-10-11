0ac484665e7f5b575f0ee87835468a8691ee9dcc
Emmanuel Chucks <echucks19@gmail.com>
2017-10-11 20:57:37 +0000

    Hosted fonts locally

--- js/contact.js
+++ js/contact.js
@@ -1,33 +1,39 @@
-$(function() {
+$(() => {
     // Get the form.
-    var form = $("#contact-form");
+    const form = $("#contact-form");
 
     // Get the messages div.
-    var formMessages = $("#form-messages");
+    const formMessages = $("#form-messages");
 
     // TODO: The rest of the code will go here...
-    $(form).submit(function(event) {
-    // Stop the browser from submitting the form.
+    $(form).submit((event) => {
+        // Stop the browser from submitting the form.
         event.preventDefault();
-        $("#submit-btn").addClass("fade-blue").val("Sending...");
+        $("#submit-btn")
+            .addClass("fade-blue")
+            .val("Sending...");
 
-        var formData = $(form).serialize();
+        const formData = $(form).serialize();
 
         // Submit the form using AJAX.
         $.ajax({
             url: "https://formspree.io/echucks19@gmail.com",
             method: "POST",
             data: JSON.stringify(formData),
-            dataType: "json"
+            dataType: "json",
         })
-            .done(function(response) {
+            .done((response) => {
                 // Make sure that the formMessages div has the 'success' class.
-                $(formMessages).removeClass("alert-danger");
-                $(formMessages).addClass("alert-success");
-                $("#submit-btn").removeClass("fade-blue").val("Send message");
+                $(formMessages).removeClass("alert alert-danger");
+                $(formMessages).addClass("alert alert-success");
+                $("#submit-btn")
+                    .removeClass("fade-blue")
+                    .val("Send message");
 
                 // Set the message text.
-                $(formMessages).text("Thanks for your message! I will reply shortly.");
+                $(formMessages).text(
+                    "Thanks for your message! I will reply shortly.",
+                );
 
                 // Clear the form.
                 $("#name").val("");
@@ -35,20 +41,16 @@ $(function() {
                 $("#email").val("");
                 $("#message").val("");
             })
-            .fail(function(data) {
+            .fail((data) => {
                 // Make sure that the formMessages div has the 'error' class.
-                $(formMessages).removeClass("alert-success");
-                $(formMessages).addClass("alert-danger");
+                $(formMessages).removeClass("alert alert-success");
+                $(formMessages).addClass("alert alert-danger");
                 $("#submit-btn").val("Sending failed...");
 
                 // Set the message text.
-                if (data.responseText !== "") {
-                    $(formMessages).text(data.responseText);
-                } else {
-                    $(formMessages).text(
-                        "Your message could not be sent. Please refresh the page and try again."
-                    );
-                }
+                $(formMessages).text(
+                    "Your message could not be sent. Please refresh the page and try again.",
+                );
             });
     });
 });
